ff581c110522deeae72676cf16b90e32168eb97a
faisalagus <faisal.atp@gmail.com>
2018-01-28 16:27:26 +0700

    Initial Chatbot

--- facebook/views.py
+++ facebook/views.py
@@ -1,11 +1,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 def facebook_callback(request):
-    return JsonResponse({
-        "test" : "Test"
-    })
+    VERIFY_TOKEN = "faisalkasepfaisalkasep"
+    mode = request.GET.get('hub.mode')
+    token = request.GET.get('hub.verify_token')
+    challenge = request.GET.get('hub.challenge')
+    if mode!=None and token!=None:
+        if mode == 'subscribe' and token == VERIFY_TOKEN:
+            return HttpResponse(challenge)
+        else:
+            return HttpResponse("Failed")
+    return HttpResponse("Callback")
