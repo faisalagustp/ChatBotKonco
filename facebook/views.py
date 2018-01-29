8c4cf4f4689723be88f9eb7ed05a1f61708e56a5
faisalagus <faisal.atp@gmail.com>
2018-01-29 22:08:34 +0700

    Initial Chatbot

--- facebook/views.py
+++ facebook/views.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import json
+
 from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
 
@@ -22,6 +24,6 @@ def facebook_callback(request):
                 return HttpResponse("Failed")
         return HttpResponse("Callback")
     elif request.method=="POST":
-        print(request.body)
+        print(json.loads(request.body))
         PAGE_TOKEN = "EAANWDfOtda8BAPsjZAgMmUcVvjZBKoOq3kxZBbNHMIRNHxGo0ZAZArae0FZBKkxuRCNcszoF3ZB3XkZBfvgcIjzUmWleiZBc5b3gmMBGFNvh3tpYOrkfGf0k8ItuMKbbqP6KxFkMZCe2Jx9BK1QgL8oRD4Xgp0wkhqnGm1BeNA3j5hlAZDZD"
         return HttpResponse("Failed")
