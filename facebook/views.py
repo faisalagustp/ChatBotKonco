913e6871c5702622a08c30062d267da830ee9304
faisalagus <faisal.atp@gmail.com>
2018-01-29 22:27:54 +0700

    Initial Chatbot

--- facebook/views.py
+++ facebook/views.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 import json
+import urllib
 
 from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
@@ -28,10 +29,27 @@ def facebook_callback(request):
         if data_request["object"] == "page":
             for entry in data_request["entry"]:
                 for messaging in entry["messaging"]:
-                    print(messaging["message"]["text"])
+                    send_response(messaging["message"]["text"],messaging["sender"]["id"])
+                    return HttpResponse("")
         else:
             return HttpResponse("Not recognized")
-
-
-        PAGE_TOKEN = "EAANWDfOtda8BAPsjZAgMmUcVvjZBKoOq3kxZBbNHMIRNHxGo0ZAZArae0FZBKkxuRCNcszoF3ZB3XkZBfvgcIjzUmWleiZBc5b3gmMBGFNvh3tpYOrkfGf0k8ItuMKbbqP6KxFkMZCe2Jx9BK1QgL8oRD4Xgp0wkhqnGm1BeNA3j5hlAZDZD"
         return HttpResponse("Failed")
+
+def send_response(message, sender_id):
+    PAGE_TOKEN = "EAANWDfOtda8BAPsjZAgMmUcVvjZBKoOq3kxZBbNHMIRNHxGo0ZAZArae0FZBKkxuRCNcszoF3ZB3XkZBfvgcIjzUmWleiZBc5b3gmMBGFNvh3tpYOrkfGf0k8ItuMKbbqP6KxFkMZCe2Jx9BK1QgL8oRD4Xgp0wkhqnGm1BeNA3j5hlAZDZD"
+
+    request_body = {
+        "recipient": {
+            "id": sender_id
+        },
+        "message": message
+    }
+
+    try:
+        req = urllib.request.Request("https://graph.facebook.com/v2.6/me/messages", data=request_body,
+                                     headers={'content-type': 'application/json', "access_token":PAGE_TOKEN})
+        response = urllib.request.urlopen(req)
+    except Exception as e:
+        print (e)
+
+
