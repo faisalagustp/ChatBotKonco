5b7f9ddcbf31b8a22dd7c683c3390dad33318ad9
faisalagus <faisal.atp@gmail.com>
2018-01-30 21:37:42 +0700

    Initial Chatbot

--- linebot/views.py
+++ linebot/views.py
@@ -1,5 +1,7 @@
 import json
+import urllib
 
+import requests
 from django.http import HttpResponse
 from django.shortcuts import render
 
@@ -10,5 +12,27 @@ from django.views.decorators.csrf import csrf_exempt
 def line_callback(request):
     if request.method == "POST":
         data_request = json.loads(request.body.decode('utf-8'))
-        print(data_request)
+        send_message(data_request["events"][0]["replyToken"],data_request["events"][0]["message"]["text"])
     return HttpResponse("clash Clawk")
+
+def send_message(reply_token,text):
+    try:
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer J2Lzw9hQNkaW8fY5MkJQyb+MdqihHXjGCE0iVDqi5cJGf6Ww+pSx5QTnL5I03SIeHuNYi2sz5a9QMm8BAiESS7Te2WwwbyiQ8EZ9dzym7mlBKcKL8nscfJnF4g7Pq8H077TBFM4rrPZV+1pXFSd0AQdB04t89/1O/w1cDnyilFU=",
+        }
+        data = json.dumps({
+            "replyToken": reply_token,
+            "messages": [
+                {
+                    "type":"text",
+                    "text":text
+                }
+            ]
+        })
+        r = requests.post("https://api.line.me/v2/bot/message/reply", headers=headers, data=data)
+        if r.status_code != 200:
+            print(r.status_code)
+            print(r.text)
+    except Exception as e:
+        print(e)
