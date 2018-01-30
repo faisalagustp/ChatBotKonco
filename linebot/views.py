1211c289ec5ad943f01957ee8337eb7714247c8f
faisalagus <faisal.atp@gmail.com>
2018-01-30 21:28:04 +0700

    Initial Chatbot

--- linebot/views.py
+++ linebot/views.py
@@ -1,7 +1,14 @@
+import json
+
 from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.decorators.csrf import csrf_exempt
 
+@csrf_exempt
 def line_callback(request):
+    if request.method == "POST":
+        data_request = json.loads(request.body.decode('utf-8'))
+        print(data_request)
     return HttpResponse("clash Clawk")
