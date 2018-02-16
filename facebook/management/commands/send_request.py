1b61fb1d0b640fe9fafccba6e7b1fe1c56f06d1e
faisalagus <faisal.atp@gmail.com>
2018-02-16 20:55:31 +0700

    Thanks Thanks

--- facebook/management/commands/send_request.py
+++ facebook/management/commands/send_request.py
@@ -13,6 +13,7 @@ class Command(BaseCommand):
         posts = models.scheduled_post_report.objects.filter(user_account__type="facebook").exclude(status="done")\
             .filter(scheduled_post__datetime__lte=datetime.now()).filter(user_account__short_memory="")
         for post in posts:
+            print("Masuk Satu")
             text = str(post.scheduled_post.post_text).replace("[name]",post.user_account.name)
             send_response(text,post.user_account.chat_id,"")
             post.datetime_sended = datetime.now()
@@ -23,6 +24,7 @@ class Command(BaseCommand):
         surveys = models.survey_submission.objects.filter(user_account__type="facebook").exclude(status="done")\
             .filter(survey__datetime__lte=datetime.now()).filter(user_account__short_memory="").exclude(status="on progress")
         for survey in surveys:
+            print("Masuk Dua")
             text = str(survey.survey.description).replace("[name]",survey.user_account.name)
             text = survey.survey.survey_name + "\n\n" + text + "\n\n Tekan ya untuk melanjutkan, tekan tidak untuk tidak melanjutkan"
             send_response(text, survey.user_account.chat_id, "ya|tidak")
