922846eac50fea692276edebc51d660e89b14042
faisalagus <faisal.atp@gmail.com>
2018-02-16 16:34:36 +0700

    Initial Chatbot

--- facebook/views.py
+++ facebook/views.py
@@ -95,6 +95,7 @@ def send_response(message, sender_id,options):
             ]
 
         data = json.dumps(data)
+        print(data)
         r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
         if r.status_code != 200:
             pass
